5305ebe15a2cda000d8cd977fd224c2d15a90504
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-23 12:53:32 +0200

    html and css profile and question athorization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// VUE DESTRUTTURAZIONE PER CREATEAPP
+const {createApp} = Vue ;
+
+
 // Milestone 1
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
